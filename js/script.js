ead965561a6d1f5503985d26a16451671e056ee7
Vlad Gritsenko <vladxmas@gmail.com>
2016-11-10 01:23:51 +0200

    replace the last operator with the newly pressed operator

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 (function () {
 var container = document.getElementById('container');
 var input = document.querySelector('.input');
-var operators = ['+', '/', 'X', '-'];
+var operators = ['.', '+', '/', 'x', '-'];
 
  container.onclick = function(e) {
  	var target = e.target;
@@ -9,25 +9,31 @@ var operators = ['+', '/', 'X', '-'];
 	if(target.tagName != 'SPAN') {
 	 	return;
 	}
-
  	input.innerHTML += target.innerHTML;
 	 
+	var inputVal = input.innerHTML;
+
  	if(target.innerHTML == 'C') {
  		input.innerHTML = '';
  	}
  	else if (target.innerHTML == '=') {
- 		var last = input.innerHTML;
- 		last = last.slice(0, -1);
- 		input.innerHTML = eval(last);
+ 		inputVal = inputVal.slice(0, -1);
+ 		input.innerHTML = eval(inputVal);
 	}
 		
 	
-	var first = input.innerHTML;
-	
 	for (var i = 0; i <= operators.length-2; i++) {
-		if(first[0] == operators[i]) {
+		if(inputVal[0] == operators[i]) {
 			input.innerHTML = '';
 		}
+	}
+
+	if(operators.includes(inputVal[inputVal.length - 1]) && operators.includes(inputVal[inputVal.length - 2])) {
+		var sign;
+		inputVal[inputVal.length - 2] = inputVal[inputVal.length - 1];
+		sign = inputVal[inputVal.length - 1];
+		input.innerHTML = inputVal.slice(0, -2);
+		input.innerHTML += sign;
 	}
  }
 }());
